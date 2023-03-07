e4dcc11ee4d1964a9c3fbd49dc32f84107fe95d6
lucky2588 <duwcsthang2001@gmail.com>
2023-03-07 12:03:57 +0700

    Update App.jsx
    Update full service

--- ReactJS-API/src/App.jsx
+++ ReactJS-API/src/App.jsx
@@ -1,33 +1,99 @@
 
 import reactLogo from './assets/react.svg'
 import './App.css'
-import { useState,useEffect } from 'react'
-
+import { useState, useEffect } from 'react'
 function App() {
-   // Tạo ra 2 giá trị để render ra màn hình 
-const [curentList , setList] = useState([]);
-const [curentText , setText] = useState("");
-
-
-useEffect(()=>{
-  const fetchData = async ()=>{
-    const data = await fetch("http://localhost:8083/todoApp/getList");
-    const xData = await data.json();
-    setList(xData);
-  }
-},[]);
-
- return (
-  <>
-   <ul>
-    {
-         curentList.map(e,index =>{
-          <li>{e.id}</li>
-         }
-          )
+  // Tạo ra 2 giá trị để render ra màn hình 
+  const [curentList, setList] = useState([]);
+  const [curentText, setText] = useState("");
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch("http://localhost:8083/todoApp/getList");
+      const xData = await data.json();
+      setList(xData);
     }
-   </ul>
-   </>
- )}
+    fetchData();
+  }, [curentList]);
+
+  const handlenBtnAdd = async () => {
+    if (curentText == "") {
+      return;
+    }
+    fetch('http://localhost:8083/todoApp/addTodo', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ title: curentText }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setList([...curentList, ...data])
+        setText('');
+      })
+      .catch((err) =>
+        console.log(err));
+  }
+  const handlenBtnRemove = (index) => {
+    fetch(`http://localhost:8083/todoApp/removeTodo/${index}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+      .then((res) => res.json())
+      .then(
+        (data) => setList([data])
+      )
+      .catch(err =>
+        console.log(err)
+      )
+  }
+  const handlenBtnEdit = async (index, e) => {
+    setText(e)
+    if (curentText == "") {
+      return;
+    }
+    fetch(`http://localhost:8083/todoApp/editTodo`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      }, body: JSON.stringify({
+        id: index,
+        title: curentText
+      }),
+    })
+      .then((res) => res.json())
+      .then(
+        (data) => setList([data])
+      )
+      .catch(err =>
+        console.log(err)
+      )
+  }
+  return (
+    <>
+      <h2>Todo App </h2>
+      <input type="text" value={curentText} placeholder=" Your Text ..." onChange={e => setText(e.target.value)} />
+      <button onClick={handlenBtnAdd} style={{ backgroundColor: "coral" }}>ADD </button>
+      <ul>
+        {
+          curentList.map((e, index) => (
+            <li key={index} style={{ liststyle: "none" }}>
+              <input type="radio" />
+              <span>{e.title}</span>
+              <button onClick={() => handlenBtnEdit(index, e.title)} style={{ backgroundColor: 'blueviolet' }}>Edit</button>
+              <button onClick={() => handlenBtnRemove(index)} style={{ backgroundColor: "bisque" }}>Remove</button>
+            </li>
+          )
+          )
+        }
+      </ul>
+
+
+
+    </>
+  )
+}
 
 export default App
