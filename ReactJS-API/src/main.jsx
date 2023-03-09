155ac021466255f313f00f2a45d8b1c0ed7d1aee
lucky2588 <duwcsthang2001@gmail.com>
2023-03-09 18:47:30 +0700

    Update main.jsx

--- ReactJS-API/src/main.jsx
+++ ReactJS-API/src/main.jsx
@@ -4,9 +4,13 @@ import App from './App'
 import "bootstrap/dist/css/bootstrap.min.css";
 import './index.css'
 import App2 from './App2';
+import AppUser from './AppUser';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+      </BrowserRouter>
   </React.StrictMode>,
 )
