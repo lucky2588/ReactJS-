1a2fc889e49113b6361aea06bc2c2be97ed32e62
lucky2588 <duwcsthang2001@gmail.com>
2023-03-14 21:28:51 +0700

    Update App.jsx

--- ReactJS-API/src/App.jsx
+++ ReactJS-API/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import NotFound from './not-found/NotFound'
 import UserCreate from './user/UserCreate'
-import UserDeteil from './user/UserDetail'
+import UserDetail1 from './user/UserDetail1'
 import UserList from './user/UserList'
 
 
@@ -17,7 +17,7 @@ function App() {
                 <Route path="/users">
                     <Route index element={<UserList/>}></Route>
                     <Route path="create" element={<UserCreate/>}></Route>
-                    <Route path=":userID" element={<UserDeteil/>}></Route>
+                    <Route path=":userID" element={<UserDetail1/>}></Route>
                 </Route>
                 <Route path="*" element={<NotFound />}>
                 </Route>
